fcb42104b17af7e777237616731c9839c96730cc
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-10-26 20:56:48 -0300

    fix meta tags and new icon

--- theme.config.js
+++ theme.config.js
@@ -12,72 +12,64 @@ export default {
   head: (
     <>
       <meta name="msapplication-TileColor" content="#ffffff" />
-
       <meta name="theme-color" content="#ffffff" />
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
       <meta httpEquiv="Content-Language" content="es" />
-
-      <meta name="description" content="Curso Front-end" />
-
-      <meta name="og:description" content="Curso Front-end" />
-
-      <meta name="twitter:card" content="summary_large_image" />
-
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-
-      <meta
-        name="twitter:site:domain"
-        content="curso-front-end-two.vercel.app"
-      />
-
-      <meta
-        name="twitter:url"
-        content="https://curso-front-end-two.vercel.app/"
-      />
-
-      <meta name="og:title" content="Curso Front-end" />
-
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-
       <meta name="apple-mobile-web-app-title" content="Curso Front-end" />
 
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-
+      <link rel="apple-touch-icon" sizes="256x256" href="/colegio256.png" />
       <link
         rel="icon"
         type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
+        sizes="256x256"
+        href="/colegio256.png"
       />
-
+      <link rel="icon" type="image/png" sizes="32x32" href="/colegio32.png" />
       <link
         rel="icon"
         type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
+        sizes="128x128"
+        href="/colegio128.png"
+      />
+      <link rel="icon" type="image/png" sizes="16x16" href="/colegio16.png" />
+      <meta name="msapplication-TileImage" content="/colegio256.png" />
+
+      <title>Curso Front-end</title>
+      <meta name="title" content="Curso Front-end" />
+      <meta
+        name="description"
+        content="Proyecto para que conozcas desde conceptos básicos hasta un nivel avanzado de front-end de aplicaciones web. En este proyecto se abarca JavaScript, HTML, CSS, React y un poco de Redux, con explicaciones, ejemplos, demostraciones y actividades."
       />
 
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://cursofrontend.vercel.app/" />
+      <meta property="og:title" content="Curso Front-end" />
+      <meta
+        property="og:description"
+        content="Proyecto para que conozcas desde conceptos básicos hasta un nivel avanzado de front-end de aplicaciones web. En este proyecto se abarca JavaScript, HTML, CSS, React y un poco de Redux, con explicaciones, ejemplos, demostraciones y actividades."
+      />
+      <meta
+        property="og:image"
+        content={`${defaultProduct?.image_1.thumbnail}`}
       />
 
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://cursofrontend.vercel.app/"
       />
-
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta property="twitter:title" content="Curso Front-end" />
+      <meta
+        property="twitter:description"
+        content="Proyecto para que conozcas desde conceptos básicos hasta un nivel avanzado de front-end de aplicaciones web. En este proyecto se abarca JavaScript, HTML, CSS, React y un poco de Redux, con explicaciones, ejemplos, demostraciones y actividades."
+      />
+      <meta
+        property="twitter:image"
+        content={`${defaultProduct?.image_1.thumbnail}`}
+      />
     </>
   ),
   search: true,
